e0e8a05a2e63a4c98dfd766f8299d43c46219ac1
Itzik Bar-David <itzik6@gmail.com>
2022-04-27 10:08:13 +0300

    Add react routing and blank settings page + add bidirectional links between application and GitHub repo + add CI/CD badge to README file (#12)

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faSync, faCodePullRequest} from '@fortawesome/free-solid-svg-icons';
+import {faSync, faCodePullRequest, faKey} from '@fortawesome/free-solid-svg-icons';
+import {faGithub} from '@fortawesome/free-brands-svg-icons';
 import React, {useState} from 'react';
 import './App.css';
 import {fetchOpenPullRequestsMultiRepo, groupPullRequests} from "./services/bitbucket";
 import useLocalStorage from "use-local-storage";
-import {PullRequest} from "./components/PullRequest";
+import {PullRequestsPage} from "./components/pull-requests/PullRequestsPage";
+import {BrowserRouter, Link, Navigate, Route, Routes} from "react-router-dom";
+import {AuthPage} from "./components/auth/AuthPage";
 
 function App() {
   // Currently, the settings must be provided by the user by manually set the local storage
   // in the future, we would like to have it via GUI
   const [settings] = useLocalStorage<Settings|null>('settings', null);
 
+  const auth = settings?.auth;
+
   const [pullRequests, setPullRequests] = useLocalStorage<BitbucketPullRequest[]>('pull-requests', []);
   const groupedPullRequests: BitbucketGroupedPullRequest[] = groupPullRequests(pullRequests);
-
-  const [searchFilter, setSearchFilter] = useState("");
 
   const [isLoading, setIsLoading] = useState(false);
 
@@ -22,49 +25,41 @@ function App() {
     if (isLoading) {
       return;
     }
-    if (!settings) {
+    if (!auth) {
       console.warn('Settings are missing. Set them and re-run.');
       return;
     }
 
     setIsLoading(true);
     try {
-      let list = await fetchOpenPullRequestsMultiRepo(settings.auth, settings.repositories);
+      let list = await fetchOpenPullRequestsMultiRepo(auth, settings.repositories);
       setPullRequests(list);
     } finally {
       setIsLoading(false);
     }
   }
 
-  function isInFilter(item: BitbucketGroupedPullRequest) {
-    return searchFilter === '' || item.source.includes(searchFilter) || item.source.includes(searchFilter);
-  }
-
-  let appBody;
-  if (groupedPullRequests.length > 0) {
-    appBody = groupedPullRequests
-      .filter(item => isInFilter(item))
-      .map(item => (
-      <PullRequest key={`${item.source}__${item.destination}`} data={item}/>
-    ));
-  } else {
-    appBody = 'No information available';
-  }
-
   return (
     <div className="App">
-      <header className="App-header">
-        <div className="App-header-title">
-          <FontAwesomeIcon icon={faCodePullRequest} /> Multi-Repo PR Status
-        </div>
-        <div className="App-header-buttons">
-          <button onClick={sync}><FontAwesomeIcon icon={faSync} spin={isLoading} /> Sync</button>
-        </div>
-      </header>
-      <div className="App-body">
-        <input placeholder="Search..." value={searchFilter} onChange={(e) => setSearchFilter(e.target.value)} />
-        {appBody}
-      </div>
+      <BrowserRouter>
+        <header className="App-header">
+          <div className="App-header-title">
+            <Link to="/"><FontAwesomeIcon icon={faCodePullRequest} /> Multi-Repo PR Status</Link>
+            <br />
+            <a href="https://github.com/algosec/multi-repo-pr-status" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} /> GitHub repo</a>
+          </div>
+          { auth &&
+            <div className="App-header-buttons">
+              <button onClick={sync}><FontAwesomeIcon icon={faSync} spin={isLoading}/> Sync</button>
+              <Link to="/auth"><FontAwesomeIcon icon={faKey}/> Authentication</Link>
+            </div>
+          }
+        </header>
+        <Routes>
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="/" element={auth ? <PullRequestsPage groupedPullRequests={groupedPullRequests} /> : <Navigate to="/auth" />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }

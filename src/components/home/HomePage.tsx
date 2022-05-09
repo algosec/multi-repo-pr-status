e2a2510f2373b9e51a8bab17d083c58e53d810d5
Itzik Bar-David <itzik6@gmail.com>
2022-05-09 09:38:14 +0300

    Add home page that explains about the application

---
+++ src/components/home/HomePage.tsx
@@ -0,0 +1,44 @@
+import {AuthPanel} from "./auth/AuthPanel";
+import {DataSourceInfo} from "../../services/DataSourceProvider";
+import './HomePage.css';
+import {faFileShield, faArrowsToEye, faWandMagicSparkles} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import React from "react";
+
+interface HomePageProps {
+  currentDataSourceInfo?: DataSourceInfo;
+  updateDataSourceInfo: (dataSourceInfo: DataSourceInfo) => void;
+}
+
+export function HomePage(props: HomePageProps) {
+  return (
+    <div>
+      <div className="Sub-header">
+        <AuthPanel updateDataSourceInfo={props.updateDataSourceInfo} currentDataSourceInfo={props.currentDataSourceInfo} />
+      </div>
+      <div className="box-holder">
+          <div className="box">
+            <div className="box-header">
+              <h1>Centralized PR view</h1>
+              <FontAwesomeIcon icon={faArrowsToEye} className="icon" />
+              <div className="content">Get a <b>bird's eye view</b> for related pull-requests from all GIT repositories you have access to. You can <b>easily share</b> set of PRs with others.</div>
+            </div>
+          </div>
+          <div className="box">
+            <div className="box-header">
+              <h1>Bulk Operations</h1>
+              <FontAwesomeIcon icon={faWandMagicSparkles} className="icon" />
+              <div className="content">Perform <b>bulk operations</b> on related pull-requests from multiple repositories such as commenting, merging and declining.</div>
+            </div>
+          </div>
+          <div className="box">
+            <div className="box-header">
+              <h1>Secured</h1>
+              <FontAwesomeIcon icon={faFileShield} className="icon" />
+              <div className="content">This application is a <b>client side only</b> (no backend), so your data stays secured between your GIT host provider and your laptop.</div>
+            </div>
+          </div>
+      </div>
+    </div>
+  );
+}

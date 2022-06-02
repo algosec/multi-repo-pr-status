baf4003054ae40a85d6946358b4b5ef7352869b2
Itzik Bar-David <itzik6@gmail.com>
2022-06-02 09:28:14 +0300

    Fix last update "52 years ago" + fix README link

--- src/components/pull-requests/SyncStatus.tsx
+++ src/components/pull-requests/SyncStatus.tsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from "react";
 import {Moment} from "moment/moment";
+import {EMPTY_TIME} from "../../state/remoteData.slice";
 
 const UPDATE_INTERVAL = 1000;
 
@@ -7,15 +8,21 @@ interface SyncStatusProps {
   lastUpdate: Moment;
 }
 
+function computeText(lastUpdate: Moment): string {
+  if (lastUpdate.isSame(EMPTY_TIME)) {
+    return "haven't synced yet";
+  }
+
+  return `Last update ${lastUpdate.fromNow()}`;
+}
+
 export const SyncStatus = React.memo((props: SyncStatusProps) => {
-  const [lastUpdateSince, setLastUpdateSince] = useState(props.lastUpdate.fromNow());
+  const [text, setText] = useState(() => computeText(props.lastUpdate));
 
   useEffect(() => {
-    const interval = setInterval(() => setLastUpdateSince(props.lastUpdate.fromNow()), UPDATE_INTERVAL);
+    const interval = setInterval(() => setText(computeText(props.lastUpdate)), UPDATE_INTERVAL);
     return () => clearInterval(interval);
   }, [props.lastUpdate]);
 
-  return (
-      <span>Last update {lastUpdateSince}</span>
-  );
+  return <span>{text}</span>;
 });
